e2f93a868c449f7d1e35f8b32c3d6b1e3b914b71
Berker ERTAN <berkerertan@hotmail.com>
2024-06-04 04:03:15 +0300

    Added template and DataAccess Layer

---
+++ Portfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.DAL.Entities;
+
+namespace Portfolio.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Portfolio ;Trusted_Connection=true");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experince> Experinces { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<PortfolioProject> PortfolioProjects { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
---
+++ Portfolio/DAL/Entities/PortfolioProject.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.DAL.Entities
+{
+    public class PortfolioProject
+    {
+        public int PortfolioProjectId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+    }
+}
